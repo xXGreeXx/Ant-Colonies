0001a6b03ca85aceb82d64b4ab3e0c4e96965f8d
Li Smith <lrhude.elijah@gmail.com>
2017-12-26 12:24:42 -0500

    Initial Commit
    base stuff

---
+++ Ant Colony/Assets/Scripts/Dirt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dirt {
+
+    //define global variables
+    public Sprite dirtTexture = Resources.Load("dirt", typeof(Sprite)) as Sprite;
+
+    public GameObject self;
+
+    public Dirt(int x, int y, int size)
+    {
+        self = new GameObject("Dirt " + x + y);
+        self.transform.parent = GameObject.Find("Canvas").transform;
+        self.transform.position = new Vector2(x, y);
+        self.transform.localScale = new Vector2(75, 75);
+
+        SpriteRenderer renderer = self.AddComponent<SpriteRenderer>();
+        renderer.sprite = dirtTexture;
+        renderer.size = new Vector2(size, size);
+    }
+}
---
+++ Ant Colony/Assets/Scripts/MainGameHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameHandler : MonoBehaviour {
+
+    //define global variables
+    public List<Dirt> dirtBlocks = new List<Dirt>();
+
+	//start
+	void Start () {
+        GenerateGround(500, 150, 1000);
+	}
+	
+    //generate ground
+    private void GenerateGround(int width, int height, int size)
+    {
+        for (int x = -width / 2; x < width / 2; x += 3)
+        {
+            for (int y = -height / 2; y < 0; y+= 3)
+            {
+                dirtBlocks.Add(new Dirt(x, y, size));
+            }
+        }
+    }
+
+	//update
+	void Update () {
+		
+	}
+}
